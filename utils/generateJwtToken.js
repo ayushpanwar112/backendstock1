ece6fa7aff461d4f7144c144de1e23c15a133e19
SachinGusain07 <sachingusain848@gmail.com>
2025-06-10 15:49:59 +0530

    added new branch

---
+++ utils/generateJwtToken.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+export const generateJWT = (user) => {
+  const payload = {
+    userId: user._id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: "1h",
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+
+  return token;
+};
+
+export const sendTokenInCookie = (res, token) => {
+  console.log("Sending token in cookie:", token);
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: true,
+    maxAge: 3600000,
+    sameSite: "None",
+  });
+};
